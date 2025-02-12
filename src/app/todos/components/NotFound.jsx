fdde4974c8fff7199528041dc236129fd47cec73
Yogesh <yr70416@gmail.com>
2025-02-12 18:57:22 +0530

    style updated

--- src/app/todos/components/NotFound.jsx
+++ src/app/todos/components/NotFound.jsx
@@ -1,7 +1,7 @@
 
 const NotFound = () => {
     return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex items-center p-[20px] justify-center min-h-screen bg-gray-100">
         <div className="text-center p-8 bg-white shadow-lg rounded-lg max-w-sm mx-auto">
           <h2 className="text-4xl font-bold text-red-600">404 - Page Not Found</h2>
           <p className="text-gray-600 mt-4">The page you are looking for does not exist.</p>
