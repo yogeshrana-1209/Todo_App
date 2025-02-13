e02980d7472af7d3e4987fd0af6587b31ebf40ad
Yogesh <yr70416@gmail.com>
2025-02-13 13:54:50 +0530

    loading updated

--- src/app/sharedComponent/notification.jsx
+++ src/app/sharedComponent/notification.jsx
@@ -1,6 +1,15 @@
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-export const SuccessNotify = (text) => toast.success(text, { autoClose: 1000 });
+export const SuccessNotify = (text) =>
+  toast.success(text, {
+    position: "top-right",
+    autoClose: 1000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
 export const InfoNotify = (text) => toast.info(text, { autoClose: 1000 });
 export const DangerNotify = (text) => toast.error(text, { autoClose: 1000 });
