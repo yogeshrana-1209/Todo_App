2a610820f6112b1c733d171c96ca2387c1114ef0
Yogesh <yr70416@gmail.com>
2025-02-22 16:54:24 +0530

    album page added

--- src/features/todos/store/TodoSlice.js
+++ src/features/todos/store/TodoSlice.js
@@ -10,7 +10,7 @@ const initialState = {
   selectedTodo: null,
   loading: false, // Add loading state
   currentPage: 1,
-  itemsPerPage: 5,
+  itemsPerPage: 4,
 };
 
 export const fetchTodos = (page, itemsPerPage) => async (dispatch) => {
