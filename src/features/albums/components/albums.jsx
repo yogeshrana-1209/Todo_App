4c4ada0f151303c3aa15aea295361bb1a7d30077
Yogesh <yr70416@gmail.com>
2025-02-25 18:52:05 +0530

    SearchBar add Validations and other

--- src/features/albums/components/albums.jsx
+++ src/features/albums/components/albums.jsx
@@ -3,6 +3,7 @@ import { fetchAlbums, setPage, setSearchTerm, getCurrentPage, getAlbumList, getM
 import AlbumList from "./albumList";
 import { useEffect, useState, useMemo } from "react";
 import debounce from "lodash.debounce";
+import * as Yup from "yup";
 
 const Albums = () => {
   const dispatch = useDispatch();
@@ -14,6 +15,7 @@ const Albums = () => {
 
   const totalPages = maxRecords > 0 ? Math.ceil(maxRecords / limit) : 1;
   const [searchText, setSearchText] = useState("");
+  const [error, setError] = useState("");
 
   useEffect(() => {
     setSearchText("");
@@ -25,15 +27,27 @@ const Albums = () => {
     dispatch(fetchAlbums(page, searchTerm));
   }, [dispatch, page, searchTerm]);
 
+  //Validation with Yup
+  const searchSchema = Yup.string()
+    .trim()
+    .required("Search field cannot be empty")
+    .matches(/^[a-zA-Z0-9\s]+$/, "Only letters and numbers are allowed")
+    .matches(/^(?!.*\s{2,}).*$/, "No consecutive spaces allowed");
+
   const handleSearch = useMemo(
     () =>
       debounce((value) => {
-        dispatch(setSearchTerm(value));
-        dispatch(resetPage());
-        // console.log("Search Term: ", value);
-        // dispatch(setPage(1));
-        // dispatch(fetchAlbums(1, value, page, searchTerm));
-      }, 1500),
+        searchSchema
+          .validate(value)
+          .then(() => {
+            setError("");
+            dispatch(setSearchTerm(value));
+            dispatch(resetPage());
+          })
+          .catch((err) => {
+            setError(err.message);
+          });
+      }, 1000),
     [dispatch]
   );
 
@@ -44,9 +58,29 @@ const Albums = () => {
   }, [handleSearch]);
 
   const onSearchChange = (e) => {
-    setSearchText(e.target.value);
-    handleSearch(e.target.value);
+    const value = e.target.value.trimStart();
+    setSearchText(value);
+
+    if (value === "") {
+      setError("");
+      dispatch(setSearchTerm(""));
+      dispatch(resetPage());
+      return;
+    }
+
+    //Show error immediately when typing
+    searchSchema
+      .validate(value)
+      .then(() => setError(""))
+      .catch((err) => setError(err.message));
+
+    handleSearch(value.trim());
+
   };
+
+  const onSearchBlur = () => {
+    setError("");
+  }
 
   // const getPageNumbers = () => {
   //   const pages = [];
@@ -88,15 +122,44 @@ const Albums = () => {
           type="text"
           value={searchText}
           onChange={onSearchChange}
+          onBlur={onSearchBlur}
           placeholder="Search Albums"
           className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
         />
-      </div>
 
-      <AlbumList albums={albums} />
+      </div>
+      {error && (
+        <p className="text-red-600 text-md mb-3 sm:text-sm px-2 py-1 rounded-md max-w-xs sm:max-w-sm mx-auto text-center">
+          {error}
+        </p>
+      )}
+
+      {/* Show Search Results Message */}
+      {!error && searchTerm && maxRecords > 0 && (
+        <p className="text-center mb-4 text-gray-600">
+          {albums.length === 0
+            ? `No records found for '${searchTerm}'`
+            : `Total ${maxRecords} records found for '${searchTerm}'`
+          }
+        </p>
+      )}
+
+      {/* Show No Records Message */}
+      {!error && searchTerm && maxRecords === 0 && (
+        <p className="text-center text-gray-500">
+          No records found for {searchTerm}
+        </p>
+      )}
+
+      {/* Display Album List */}
+      {albums.length > 0 ? (
+        <AlbumList albums={albums}/>
+      ) : (
+        !error && !searchTerm && <p className="text-center text-gray-500">No albums found.</p>
+      )}
 
       {/* Pagination Controls */}
-      {maxRecords > 0 && (
+      {maxRecords > 0 && albums.length > 0 && (
         <div className="mt-4 flex justify-center gap-4">
           <button
             className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50"
